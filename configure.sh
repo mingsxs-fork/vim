5814ce08e63bb1051b195c1a269c0792a7e43088
银帆 <yinfan.lm@alibaba-inc.com>
2022-06-10 16:39:55 +0800

    update configure.sh and Makefile for GCC flags.

--- configure.sh
+++ configure.sh
@@ -1,1 +1,4 @@
-./configure --prefix=$HOME --with-compiledby="Ming Li(adagio.ming@gmail.com)" --with-features=huge --enable-fail-if-missing --enable-mzschemeinterp --enable-perlinterp --enable-pythoninterp --enable-python3interp --enable-tclinterp --enable-rubyinterp --enable-cscope --enable-terminal --enable-autoservername --enable-multibyte --enable-xim --enable-fontset --enable-gui=auto --with-x --with-tlib=ncurses
+./configure --prefix=/usr/local --with-compiledby="Ming Li(adagio.ming@gmail.com)" --with-features=huge \
+--enable-fail-if-missing --enable-mzschemeinterp --enable-perlinterp --enable-pythoninterp --enable-python3interp \
+--enable-tclinterp --enable-rubyinterp --enable-cscope --enable-terminal --enable-autoservername --enable-multibyte \
+--enable-xim --enable-fontset --enable-gui=auto --with-x --with-tlib=ncurses --enable-gpm
